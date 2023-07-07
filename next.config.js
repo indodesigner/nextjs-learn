539a484ebc899ea82bf15fe6786eeddaa7ae6fea
indodesigner <webdesigner@indocosmo.com>
2023-07-07 12:22:44 +0530

    yh

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    /* config options here */
-  }
-   
-  module.exports = nextConfig
+  /* config options here */
+
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+};
+
+module.exports = nextConfig;
