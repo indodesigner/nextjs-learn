c874ccfc53cc25057c88441d21d050c1b7e9a12f
indodesigner <webdesigner@indocosmo.com>
2023-10-03 10:35:02 +0530

    contact form route edit

--- app/(site)/api/contact/route.js
+++ app/(site)/api/contact/route.js
@@ -1,43 +1,5 @@
 import { NextResponse } from "next/server";
-import sgMail from "@sendgrid/mail";
-
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-export const dynamic = "auto";
-
-export const sendEmail = async (response) => {
-  const {
-    name,
-    countryCode,
-    phone,
-    email,
-    selectedPackageName,
-    selectedPackageSlug,
-    message,
-  } = response;
-
-  const href = "http://localhost:3000/packages/"; //change this to domain after hosting <<<<<<<important>
-
-  const msg = {
-    to: "webdesigner@indocosmo.com",
-    from: "webdesigner@indocosmo.com",
-    subject: `New Enquiry from ${name}`,
-    text: `
-          Name: ${name}
-          Phone: (${countryCode}) ${phone}
-          Email: ${email}
-          Interested Package: ${selectedPackageName}
-          Link to Pack: ${href}${selectedPackageSlug}
-          Message: ${message}`,
-  };
-
-  try {
-    await sgMail.send(msg);
-    console.log(`Email sent to ${to}`);
-  } catch (error) {
-    console.error(error);
-  }
-};
+import { sendEmail } from "/components/sendMail";
 
 export async function POST(request) {
   const response = await request.json();
