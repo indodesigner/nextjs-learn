f178c73e42ca2c942e58890d15e80fbbb12de17d
indodesigner <webdesigner@indocosmo.com>
2023-12-30 12:45:34 +0530

    remove motion framer-motion causing issues

--- app/(site)/components/heroCarousel.jsx
+++ app/(site)/components/heroCarousel.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Navigation, Autoplay, EffectFade } from "swiper/modules";
-import { motion, AnimatePresence } from "framer-motion";
+// import { motion, AnimatePresence } from "framer-motion";
 import { Player, Controls } from "@lottiefiles/react-lottie-player";
 import LottieArrow from "/public/lottie/down-arrow.json";
 import Link from "next/link";
@@ -54,88 +54,88 @@ const HeroCarousel = ({ slides }) => {
                 alt={slide.alt}
                 className="min-w-screen max-h-[70vh] md:max-h-screen object-cover rounded-xl md:rounded-3xl"
               ></Image>
-              <AnimatePresence>
-                <div className="absolute group flex justify-center items-center bottom-0 py-10 min-w-[100%] min-h-[100%] bg-neutral-900 bg-opacity-60 hover:bg-opacity-70 text-white px-3 transition duration-300">
-                  <span className="flex flex-col items-center">
-                    <motion.h1
-                      initial={{ opacity: 0 }}
-                      animate={{ opacity: 1 }}
-                      exit={{ opacity: 0, y: 15 }}
-                      transition={{ delay: 0.5 }}
-                      className="text-2xl sm:text-5xl lg:text-7xl font-extrabold drop-shadow-md gradient-text py-3 mb-2"
-                    >
-                      {language === "english" ? slide.title : slide.titlejp}
-                    </motion.h1>
+              {/* <AnimatePresence> */}
+              <div className="absolute group flex justify-center items-center bottom-0 py-10 min-w-[100%] min-h-[100%] bg-neutral-900 bg-opacity-60 hover:bg-opacity-70 text-white px-3 transition duration-300">
+                <span className="flex flex-col items-center">
+                  <h1
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0, y: 15 }}
+                    transition={{ delay: 0.5 }}
+                    className="text-2xl sm:text-5xl lg:text-7xl font-extrabold drop-shadow-md gradient-text py-3 mb-2"
+                  >
+                    {language === "english" ? slide.title : slide.titlejp}
+                  </h1>
 
-                    <motion.p
-                      initial={{ opacity: 0 }}
-                      animate={{ opacity: 1 }}
-                      exit={{ opacity: 0, y: 15 }}
-                      transition={{ delay: 1 }}
-                      className="mb-4 text-xs sm:text-sm md:text-md lg:text-lg font-light w-[100%] sm:max-w-[80%] md:max-w-[60%] md:block hidden"
-                    >
-                      {language === "english" ? slide.caption : slide.captionjp}
-                    </motion.p>
-                    {/* <motion.span
+                  <p
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0, y: 15 }}
+                    transition={{ delay: 1 }}
+                    className="mb-4 text-xs sm:text-sm md:text-md lg:text-lg font-light w-[100%] sm:max-w-[80%] md:max-w-[60%] md:block hidden"
+                  >
+                    {language === "english" ? slide.caption : slide.captionjp}
+                  </p>
+                  {/* <motion.span
                       initial={{ opacity: 0 }}
                       animate={{ opacity: 1 }}
                       exit={{ opacity: 0, y: 15 }}
                       transition={{ delay: 0.75 }}
                       className="bg-white py-[1px] sm:py-[1.5px] px-4 rounded-2xl mb-6 md:block hidden"
                     ></motion.span> */}
-                    <motion.span
-                      initial={{ opacity: 0 }}
-                      animate={{ opacity: 1 }}
-                      exit={{ opacity: 0, y: 15 }}
-                      transition={{ delay: 1.25 }}
-                    >
-                      <div className="grid grid-cols-2 gap-3">
-                        {path == "/"
-                          ? links.map((link, index) => (
-                              <Link
-                                key={index}
-                                href={`/${link.href}`}
-                                className="flex flex-col items-center py-2 px-4 sm:px-8 bg-neutral-300 border-2 border-neutral-100 border-opacity-10 bg-opacity-20 backdrop-blur-xl rounded-xl shadow-md hover:shadow-neutral-900 hover:-translate-y-1 transition"
-                              >
-                                {link.icon === "IndiaFlagIcon" ? (
-                                  <Image
-                                    src={IndiaFlagIcon}
-                                    width={48}
-                                    height={48}
-                                    className="w-8 sm:w-12 md:w-16 h-auto"
-                                    alt="Indian flag button icon"
-                                  ></Image>
-                                ) : (
-                                  <Image
-                                    src={JapanFlagIcon}
-                                    width={48}
-                                    height={48}
-                                    className="w-8 sm:w-12 md:w-16 h-auto"
-                                    alt="Japanese flag button icon"
-                                  ></Image>
-                                )}
-                                <h5 className="text-sm font-light sm:text-xl">
-                                  {language === "english"
-                                    ? link.name
-                                    : link.namejp}
-                                </h5>
-                              </Link>
-                            ))
-                          : null}
-                      </div>
-                    </motion.span>
-
-                    <a href="#packages" className="lg:block hidden">
-                      <Player
-                        autoplay
-                        loop
-                        src={LottieArrow}
-                        style={{ height: "64px", width: "64px" }}
-                      ></Player>
-                    </a>
+                  <span
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0, y: 15 }}
+                    transition={{ delay: 1.25 }}
+                  >
+                    <div className="grid grid-cols-2 gap-3">
+                      {path == "/"
+                        ? links.map((link, index) => (
+                            <Link
+                              key={index}
+                              href={`/${link.href}`}
+                              className="flex flex-col items-center py-2 px-4 sm:px-8 bg-neutral-300 border-2 border-neutral-100 border-opacity-10 bg-opacity-20 backdrop-blur-xl rounded-xl shadow-md hover:shadow-neutral-900 hover:-translate-y-1 transition"
+                            >
+                              {link.icon === "IndiaFlagIcon" ? (
+                                <Image
+                                  src={IndiaFlagIcon}
+                                  width={48}
+                                  height={48}
+                                  className="w-8 sm:w-12 md:w-16 h-auto"
+                                  alt="Indian flag button icon"
+                                ></Image>
+                              ) : (
+                                <Image
+                                  src={JapanFlagIcon}
+                                  width={48}
+                                  height={48}
+                                  className="w-8 sm:w-12 md:w-16 h-auto"
+                                  alt="Japanese flag button icon"
+                                ></Image>
+                              )}
+                              <h5 className="text-sm font-light sm:text-xl">
+                                {language === "english"
+                                  ? link.name
+                                  : link.namejp}
+                              </h5>
+                            </Link>
+                          ))
+                        : null}
+                    </div>
                   </span>
-                </div>
-              </AnimatePresence>
+
+                  <a href="#packages" className="lg:block hidden">
+                    <Player
+                      autoplay
+                      loop
+                      src={LottieArrow}
+                      style={{ height: "64px", width: "64px" }}
+                    ></Player>
+                  </a>
+                </span>
+              </div>
+              {/* </AnimatePresence> */}
             </div>
           </SwiperSlide>
         ))}
